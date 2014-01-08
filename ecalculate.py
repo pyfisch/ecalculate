8a09800b664de016b825dbbdea1fc9ed6297f015
Pyfisch <pyfisch@googlemail.com>
2014-01-08 19:33:38 +0100

    Use table to arrange buttons.

--- ecalculate.py
+++ ecalculate.py
@@ -10,7 +10,8 @@ from efl import elementary
 from efl.elementary.window import StandardWindow
 from efl.elementary.box import Box
 from efl.elementary.button import Button
-from efl.elementary.entry import Entry 
+from efl.elementary.entry import Entry
+from efl.elementary.table import Table
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 EXPAND_HORIZONTAL = EVAS_HINT_EXPAND, 0.0
@@ -18,32 +19,16 @@ FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
 FILL_HORIZONTAL = EVAS_HINT_FILL, 0.0
 
 
-class CBox(Box):
-    def __init__(self, *args, **kwargs):
-        if not 'horizontal' in kwargs:
-            kwargs['horizontal'] = False
-        Box.__init__(self,
-                     kwargs['window'], 
-                     horizontal=kwargs['horizontal'],
-                     size_hint_weight=EXPAND_BOTH,
-                     size_hint_align=FILL_BOTH)
-        for i in args:
-            self.pack_end(i)
-
-    def show(self):
-        Box.show(self)
-        for i in self.children:
-            i.show()
-
-
 class CButton(Button):
-    def __init__(self, window, text, action=None):
+    def __init__(self, window, text, x, y, action=None):
         Button.__init__(self, window, text=str(text),
                         size_hint_weight=EXPAND_BOTH,
                         size_hint_align=FILL_BOTH)
         self.action = action
         self.window = window
         self.callback_clicked_add(self.clicked)
+        self.window.table.pack(self, x, y, 1, 1)
+        self.show()
     
     def clicked(self, caller):
         #TODO: Probably I should move this to the Calculator class
@@ -63,59 +48,44 @@ class CButton(Button):
         self.window.display()
 
 
-class CEntry(Entry):
-    def __init__(self, window):
-        Entry.__init__(self, window,
-                       size_hint_weight=EXPAND_HORIZONTAL,
-                       size_hint_align=FILL_HORIZONTAL,
-                       single_line=True,
-                       editable=False)
-
-
 class Calculator(StandardWindow):
     def __init__(self):
         self.value1 = ''
         self.value2 = ''
         self.action = None
 	StandardWindow.__init__(self, "ECalculate", "ECalculate", autodel=True, size=(300, 300))
-        self.mainbox = Box(self, size_hint_weight=EXPAND_BOTH)
-        self.resize_object_add(self.mainbox)
-        self.mainbox.show()
-        self.entry_field = CEntry(self)
-        #TODO: A real grid should be used here.
-	grid = CBox(self.entry_field,
-                    CBox(CButton(self, 1),
-                         CButton(self, 2),
-                         CButton(self, 3),
-	                 CButton(self, '/', 'divide'),
-                         window=self,
-                         horizontal=True,
-                        ),
-                    CBox(CButton(self, 4),
-                         CButton(self, 5),
-                         CButton(self, 6),
-	                 CButton(self, '*', 'multiply'),
-                         window=self,
-                         horizontal=True,
-                        ),
-                    CBox(CButton(self, 7),
-                         CButton(self, 8),
-                         CButton(self, 9),
-	                 CButton(self, '-', 'minus'),
-                         window=self,
-                         horizontal=True,
-                        ),
-                    CBox(CButton(self, 0),
-                         CButton(self, '.', 'point'),
-                         CButton(self, '=', 'submit'),
-	                 CButton(self, '+', 'plus'),
-                         window=self,
-                         horizontal=True,
-                        ),
-                    window=self,
-        )
-        self.resize_object_add(grid)
-        grid.show()
+        # Create box
+        self.box = Box(self, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+        self.resize_object_add(self.box)
+        self.box.show()
+        # Create Input field
+        self.field = Entry(self, single_line=True, editable=False,
+                           size_hint_weight=EXPAND_HORIZONTAL,
+                           size_hint_align=FILL_HORIZONTAL)
+        self.box.pack_end(self.field)
+        self.field.show()
+        # Create table holding the buttons
+        self.table = Table(self, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+        self.box.pack_end(self.table)
+        self.table.show()
+        # Create buttons
+        CButton(self, x=0, y=0, text='1') 
+        CButton(self, x=1, y=0, text='2') 
+        CButton(self, x=2, y=0, text='3') 
+        CButton(self, x=0, y=1, text='4') 
+        CButton(self, x=1, y=1, text='5') 
+        CButton(self, x=2, y=1, text='6') 
+        CButton(self, x=0, y=2, text='7') 
+        CButton(self, x=1, y=2, text='8') 
+        CButton(self, x=2, y=2, text='9') 
+        CButton(self, x=0, y=3, text='0') 
+        CButton(self, x=4, y=0, text='/', action='divide') 
+        CButton(self, x=4, y=1, text='*', action='multiply') 
+        CButton(self, x=4, y=2, text='-', action='minus') 
+        CButton(self, x=4, y=3, text='+', action='plus') 
+        CButton(self, x=2, y=3, text='=', action='submit')
+        CButton(self, x=1, y=3, text='.', action='point')
+
         self.show()
 
     def calculate(self):
@@ -136,7 +106,7 @@ class Calculator(StandardWindow):
         self.value1 = ''
     
     def display(self):
-        self.entry_field.text = self.value2
+        self.field.text = self.value2
 
 
 if __name__ == "__main__":
